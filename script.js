ddb4a7f4822f4978bea4e1d9cdb89fd4c1926365
andrey-glazkov-frontend <dr-greenfake@yandex.ru>
2022-11-16 17:44:04 +0300

    some methods

--- script.js
+++ script.js
@@ -1,32 +1,91 @@
 
 
- const $wr = document.querySelector('[data-wr]')
- var cnt = 0
-
+const $wr = document.querySelector('[data-wr]')
+var cnt = 0
+let $buttonBoard = document.querySelector('.button_board')
 
 
 const catInfo = 'http://sb-cats.herokuapp.com/api/2/andrey-glazkov-frontend/show'
 const catAdd = 'http://sb-cats.herokuapp.com/api/2/andrey-glazkov-frontend/add'
+const catDel = 'http://sb-cats.herokuapp.com/api/2//andrey-glazkov-frontend/delete/'
+const cardAdd = 'http://sb-cats.herokuapp.com/api/2/andrey-glazkov-frontend/add'
+
+async function deleteCat(id) {
+    try {
+        const deleteResponse = await fetch(`${catDel}${id}`, {
+            method: 'DELETE',
+        })
+
+        if (deleteResponse.status !== 200) {
+            throw new Error()
+        }
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+async function catAdder(catCardInfo) {
+    try {
+        const addResponse = await fetch(cardAdd, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'aplication/json',
+            },
+            body: JSON.stringify(catCardInfo),
+        })
+        if (addResponse.status !== 200) {
+            throw new Error()
+        }
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+class API {
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl
+    }
+
+
+    async addCat(data) {
+        try {
+            const response = await fetch(`${this.baseUrl}/add`, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            })
+
+            if (response.status !== 200) {
+                throw new Error()
+            }
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+}
+
 
 // fetch(catInfo)
 //  .then((response) => response.json())
 //  .then((json) => {
 
-   
+
 //        const postsHTML = json.data.map(post => cardGenerator(post)).join("")
 //     //    $wr.insertAdjacentHTML('beforeend', postsHTML)
 //    });
 const cardGenerator = (post) => {
     const newHTML = document.createElement('div');
     newHTML.innerHTML = `
-    <div  class="card">
+    <div  class="card" data-card_id=${post.id}>
         <h3>${post.name}</h3>
-        <img src="${post.img_link}" alt="">
+        <img src="${post.img_link}" alt="${post.name}">
         <p>${post.description}</p>
         <p id="features__rate-box"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></p>
     `;
     $wr.append(newHTML);
-    
+
 }
 
 
@@ -36,16 +95,16 @@ async function waitingFetch() {
     return json
 }
 
-async function render(){
-waitingFetch()
-    
-    .then((json) => {
+async function render() {
+    waitingFetch()
 
-          const postsHTML = json.data.map(post => cardGenerator(post)).join("")
+        .then((json) => {
 
-      });
+            const postsHTML = json.data.map(post => cardGenerator(post)).join("")
 
-    }
+        });
+
+}
 render()
 const button = document.querySelector('button');
 const form = document.querySelector('#blablabla');
@@ -54,15 +113,15 @@ const $btnClose = document.querySelector('.close');
 
 
 button.addEventListener('click', () => {
-  form.classList.add('open');
-  popup.classList.add('popup_open');
+    form.classList.add('open');
+    popup.classList.add('popup_open');
 });
 
-$btnClose.addEventListener('click', ()=> {
+$btnClose.addEventListener('click', () => {
     form.classList.remove('open');
     popup.classList.remove('popup_open');
-  
-    
+
+
 });
 
 
@@ -76,7 +135,7 @@ $btnClose.addEventListener('click', ()=> {
 //     'Content-Type': 'application/json'
 //   },
 
-  
+
 //   body: JSON.stringify({
 //     id
 //     name: myName,
@@ -98,7 +157,7 @@ $btnClose.addEventListener('click', ()=> {
 //  .then((response) => response.json())
 //  .then((json) => {
 
-   
+
 //        const postsHTML = json.data.map(post => cardGenerator(post)).join("")
 //     //    $wr.insertAdjacentHTML('beforeend', postsHTML)
 //    });
@@ -117,35 +176,49 @@ $btnClose.addEventListener('click', ()=> {
 
 // const $card = document.querySelectorAll('.card')
 
-const $btnDel = document.querySelector('.btn_delete')
+
 const $btnFind = document.querySelector('.btn_find')
 
 
- function catsFinder() {
+function catsFinder() {
 
     $card.forEach(el => {
         el.innerHTML += `<input type="checkbox" class='delete'>`
     });
- }
- function catsRemover () {
-    let $chbox = document.querySelectorAll('.delete')
-    $chbox.forEach(el => {
-    if (el.checked)
-    { let deleteable = el.parentNode
-        el.parentElement.remove()
-        el.remove()
-    } else {
-        el.remove()
-    }
-});
- }
+}
 
- function inputHider() {
+
+
+function catsRemover() {
+
+    let $chbox = document.querySelectorAll('.delete')
+    $chbox.forEach((el) => {
+        if (el.checked) {
+            // let deleteable = el.parentNode
+            // el.parentElement.remove()
+            // console.log(event.target.dataset.action)
+            // let deletableId = event.target.closest('[data-card_id]')
+            let catId = el.parentElement.dataset.card_id
+            // console.log(deletableId)
+            cnt = 0
+            deleteCat(catId).then(() => {
+                el.parentElement.remove()
+
+            })
+            $buttonBoard.lastElementChild.remove()
+        } else {
+            el.remove()
+        }
+    });
+}
+
+function inputHider() {
+
     let $chbox = document.querySelectorAll('.delete')
     $chbox.forEach(el => {
         el.remove()
-});
- }
+    });
+}
 
 
 // $btnFind.addEventListener('click', e => {
@@ -157,20 +230,70 @@ const $btnFind = document.querySelector('.btn_find')
 //     }
 // })
 
+
+
+function deleteButtonAdd() {
+
+    // $buttonBoard.innerHTML += '<button class="btn_delete">Убрать котика</button>'
+    let addedButton = document.createElement('button')
+    addedButton.innerHTML = 'Убрать котика'
+    addedButton.classList.add('btn_delete')
+    $buttonBoard.append(addedButton)
+    const $card = document.querySelectorAll('.card')
+    const $btnDel = document.querySelector('.btn_delete')
+    $btnDel.addEventListener('click', catsRemover)
+    $card.forEach(el => {
+        el.innerHTML += `<input type="checkbox" class='delete'>`
+    });
+
+}
+
 $btnFind.addEventListener('click', e => {
     cnt++;
-    if (cnt%2 == 1) {
-        const $card = document.querySelectorAll('.card')
-        console.log($card)
-        $card.forEach(el => {
-            el.innerHTML += `<input type="checkbox" class='delete'>`
-        });
+    console.log(cnt)
+    if (cnt % 2 == 1) {
+        deleteButtonAdd()
+
+
     } else {
         inputHider()
+        $buttonBoard.lastElementChild.remove()
     }
+
 })
 
- $btnDel.addEventListener('click',catsRemover)
+
+// document.forms.add_cat.addEventListener('submit', (event) => {
+//     event.preventDefault()
+
+//     const data = Object.fromEntries(new FormData(event.target).entries())
+
+//     data.id = +data.id
+//     data.rate = +data.rate
+//     data.favorite = data.favorite === 'on'
+//     console.log(data)
+//     catAdder(data).then(() => {
+//         cardGenerator(data)
+
+//     }).catch(alert)
+// })
+
+const api = new API('http://sb-cats.herokuapp.com/api/2/andrey-glazkov-frontend')
+document.forms.add_cat.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const data = Object.fromEntries(new FormData(event.target).entries())
+    data.name = data.Name
+    data.id = +data.id
+    data.rate = +data.rate
+    data.favorite = data.favorite === 'on'
+    form.classList.remove('open');
+    popup.classList.remove('popup_open');
+    api.addCat(data).then(() => {
+        cardGenerator(data)
+    }).catch(alert)
+
+})
 
 //  fetch(catInfo)
 //  .then((response) => response.json())
@@ -181,3 +304,4 @@ $btnFind.addEventListener('click', e => {
 
 
 
+// 20 56 массовое удаление   сделать валидацию формы кнопки звездочки? поправить без пустых мест, разобраться с кнопкой
